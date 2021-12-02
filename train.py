d58c722e8fa7c88234d5e6472fec6165c1148974
Devin Capriola <devincapriola@gmail.com>
2021-12-01 21:14:19 -0500

    Creating the bots

--- train.py
+++ train.py
@@ -3,3 +3,12 @@
 # Importing the libraries
 import numpy as np
 from environment import Environment
+
+# Creating the bots
+# dna_size = 3,1,2,0
+# fitness_size = 3,1,2,0
+class Route():
+    def __init__(self, dnaLength):
+        self.dnaLength = dnaLength
+        self.dna = list()
+        self.distance = 0
