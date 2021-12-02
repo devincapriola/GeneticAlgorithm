a7f445ea444bd22dfdaadbf7bb430e6db9f84a37
Devin Capriola <devincapriola@gmail.com>
2021-12-01 21:18:25 -0500

    Initialize the random dna

--- train.py
+++ train.py
@@ -5,10 +5,16 @@ import numpy as np
 from environment import Environment
 
 # Creating the bots
-# dna_size = 3,1,2,0
-# fitness_size = 3,1,2,0
 class Route():
     def __init__(self, dnaLength):
         self.dnaLength = dnaLength
         self.dna = list()
         self.distance = 0
+
+        # Initialize the random dna
+        for i in range(self.dnaLength - 1):
+            rnd = np.random.randint(1, self.dnaLength)
+            while rnd in self.dna:
+                rnd = np.random.randint(1, self.dnaLength)
+            self.dna.append(rnd)
+        self.dna.append(0)
